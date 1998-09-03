94cb1bf01ca16aade6b3628729a05797e38c4f2a
Niels Möller <nisse@lysator.liu.se>
1998-09-03 08:33:37 +0200

    New files
    Rev: src/symmetric/Makefile.in:1.1
    Rev: src/symmetric/cast.c:1.1
    Rev: src/symmetric/cast_sboxes.h:1.1
    Rev: src/symmetric/configure.in:1.1
    Rev: src/symmetric/desCode.h:1.1
    Rev: src/symmetric/desKerb.c:1.1
    Rev: src/symmetric/desQuick.c:1.1
    Rev: src/symmetric/desTest.c:1.1
    Rev: src/symmetric/desUtil.c:1.1
    Rev: src/symmetric/descore.README:1.1
    Rev: src/symmetric/desdata.c:1.1
    Rev: src/symmetric/desinfo.h:1.1
    Rev: src/symmetric/idea.c:1.1
    Rev: src/symmetric/include/RCSID.h:1.1
    Rev: src/symmetric/include/cast.h:1.1
    Rev: src/symmetric/include/crypto_types.h:1.1
    Rev: src/symmetric/include/des.h:1.1
    Rev: src/symmetric/include/idea.h:1.1
    Rev: src/symmetric/include/md5.h:1.1
    Rev: src/symmetric/include/rc4.h:1.1
    Rev: src/symmetric/include/sha.h:1.1
    Rev: src/symmetric/install-sh:1.1
    Rev: src/symmetric/md5.c:1.1
    Rev: src/symmetric/rc4.c:1.1
    Rev: src/symmetric/sha.c:1.1

---
+++ desKerb.c
@@ -0,0 +1,31 @@
+/*
+ *	des - fast & portable DES encryption & decryption.
+ *	Copyright (C) 1992  Dana L. How
+ *	Please see the file `README' for the complete copyright notice.
+ */
+
+#include "des.h"
+
+#include "RCSID.h"
+RCSID2(desKerb_cRcs, "$Id$");
+
+/* permit the default style of des functions to be changed */
+
+DesFunc *DesCryptFuncs[2] = { DesSmallFipsDecrypt, DesSmallFipsEncrypt };
+
+/* kerberos-compatible key schedule function */
+
+int
+des_key_sched(unsigned INT8 *k, unsigned INT32 *s)
+{
+	return DesMethod(s, k);
+}
+
+/* kerberos-compatible des coding function */
+
+int
+des_ecb_encrypt(unsigned INT8 *s, unsigned INT8 *d, unsigned INT32 *r, int e)
+{
+	(*DesCryptFuncs[e])(d, r, s);
+	return 0;
+}
---
+++ rc4.c
@@ -0,0 +1,48 @@
+/* rc4.c
+ *
+ */
+
+#include "crypto_types.h"
+#include <rc4.h>
+
+#ifdef RCSID
+RCSID("$Id$");
+#endif
+
+#define SWAP(a,b) do { int _t = a; a = b; b = _t; } while(0)
+
+void rc4_set_key(struct rc4_ctx *ctx, const unsigned INT8 *key, INT32 len)
+{
+  register unsigned INT8 j; /* Depends on the eight-bitness of these variables. */
+  unsigned i;
+  INT32 k;
+
+  /* Initialize context */
+  i = 0;
+  do ctx->S[i] = i; while (++i < 256);
+
+  /* Expand key */
+  i = j = k = 0;
+  do {
+    j += ctx->S[i] + key[k];
+    SWAP(ctx->S[i], ctx->S[j]);
+    k = (k+1) % len; /* Repeat key if needed */
+  } while(++i < 256);
+  
+  ctx->i = ctx->j = 0;
+}
+
+void rc4_crypt(struct rc4_ctx *ctx, unsigned INT8 *dest, const unsigned INT8 *src, INT32 len)
+{
+  register unsigned INT8 i, j; /* Depends on the eight-bitness of these variables */
+
+  i = ctx->i; j = ctx->j;
+  while(len--)
+    {
+      i++;
+      j += ctx->S[i];
+      SWAP(ctx->S[i], ctx->S[j]);
+      *dest++ = *src++ ^ ctx->S[ (ctx->S[i] + ctx->S[j]) & 0xff ];
+    }
+  ctx->i = i; ctx->j = j;
+}
