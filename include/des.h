2d429c0f1b5a0258979660151b99eefe320b625e
Niels Möller <nisse@lysator.liu.se>
1998-12-31 05:28:34 +0100

    Constified
    Rev: src/symmetric/include/des.h:1.4

--- include/des.h
+++ include/des.h
@@ -1,7 +1,8 @@
 /*
  *	des - fast & portable DES encryption & decryption.
  *	Copyright (C) 1992  Dana L. How
- *	Please see the file `README' for the complete copyright notice.
+ *	Please see the file `../lib/descore.README' for the complete copyright
+ *	notice.
  *
  * Slightly edited by Niels Möller, 1997
  */
@@ -22,9 +23,9 @@ RCSID2(desCore_hRcs, "$Id$");
 typedef UINT8 DesData[DES_BLOCKSIZE];
 typedef UINT32 DesKeys[DES_EXPANDED_KEYLEN];
 
-typedef void DesFunc(UINT8 *d, UINT32 *r, const UINT8 *s);
+typedef void DesFunc(UINT8 *d, const UINT32 *r, const UINT8 *s);
 
-extern int DesMethod(UINT32 *method, UINT8 *k);
+extern int DesMethod(UINT32 *method, const UINT8 *k);
 extern void DesQuickInit(void);
 extern void DesQuickDone(void);
 extern DesFunc DesQuickCoreEncrypt;
@@ -37,7 +38,7 @@ extern DesFunc DesSmallCoreDecrypt;
 extern DesFunc DesSmallFipsDecrypt;
 
 extern DesFunc *DesCryptFuncs[2];
-extern int des_key_sched(UINT8 *k, UINT32 *s);
-extern int des_ecb_encrypt(UINT8 *s, UINT8 *d, UINT32 *r, int e);
+extern int des_key_sched(const UINT8 *k, UINT32 *s);
+extern int des_ecb_encrypt(const UINT8 *s, UINT8 *d, const UINT32 *r, int e);
 
 #endif /*  DES_H_INCLUDED */
