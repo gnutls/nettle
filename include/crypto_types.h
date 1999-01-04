a23fa5b00c9d4f27063bd759e6bd152142f1f0fd
Niels Möller <nisse@lysator.liu.se>
1999-01-04 10:44:18 +0100

    Reorganized lsh.h and lsh_Types.h.
    Rev: src/symmetric/include/crypto_types.h:1.6

--- include/crypto_types.h
+++ include/crypto_types.h
@@ -13,52 +13,14 @@
 # define UINT8 unsigned INT8
 #else  /* !PIKE */
 
-/* FIXME: Reorganize the header files for definitions. This stuff
- * should probably live in lsh_types.h, and object definitions should
- * move into a separate header file.
- *
- * FIXME: some of the crypto implementations could well use the
- * READ_UINT32 and WRITE_UINT32 macros. */
-
 # ifdef LSH
-#  ifdef HAVE_CONFIG_H
-#   include "config.h"
-#  endif
-#  if SIZEOF_SHORT >= 4
-#   define UINT32 unsigned short
-#  elif SIZEOF_INT >= 4
-#   define UINT32 unsigned int
-#  elif SIZEOF_LONG >= 4
-#   define UINT32 unsigned long
-#  else
-#   error No suitable type found to use for UINT32
-#  endif /* UINT32 */
-
-#  if SIZEOF_SHORT >= 2
-#   define UINT16 unsigned short
-#  elif SIZEOF_INT >= 2
-#   define UINT16 unsigned int
-#  else
-#   error No suitable type found to use for UINT16
-#  endif  /* UINT16 */
-
-#  define UINT8 unsigned char
+#  include "lsh_types.h"
 
 # else /* !LSH */
 #  define UINT32 unsigned long
 #  define UINT16 unsigned short
 #  define UINT8 unsigned char
 # endif /* !PIKE */
-#endif
-
-#ifdef __GNUC__
-#define NORETURN __attribute__ ((noreturn))
-#define PRINTF_STYLE(f, a) __attribute__ ((format(printf, f, a)))
-#define UNUSED __attribute__ ((unused))
-#else
-#define NORETURN
-#define PRINTF_STYLE(f, a)
-#define UNUSED
 #endif
 
 #endif /* CRYPTO_TYPES_H_INCLUDED */
