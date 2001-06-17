6324117397258f756ac14c7562beb6141188134b
Niels Möller <nisse@lysator.liu.se>
2001-06-17 18:33:09 +0200

    Fixed copyright notice.
    Rev: src/nettle/aes.c:1.3
    Rev: src/nettle/aes.h:1.3
    Rev: src/nettle/arcfour.c:1.2
    Rev: src/nettle/arcfour.h:1.2
    Rev: src/nettle/blowfish.h:1.5
    Rev: src/nettle/cast128.c:1.2
    Rev: src/nettle/cast128.h:1.2
    Rev: src/nettle/des.c:1.3
    Rev: src/nettle/des.h:1.3
    Rev: src/nettle/macros.h:1.3
    Rev: src/nettle/md5.c:1.2
    Rev: src/nettle/md5.h:1.2
    Rev: src/nettle/serpent.h:1.4
    Rev: src/nettle/sha1.c:1.3
    Rev: src/nettle/sha1.h:1.2
    Rev: src/nettle/twofish.c:1.3
    Rev: src/nettle/twofish.h:1.3

--- aes.h
+++ aes.h
@@ -18,7 +18,7 @@
  * License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+ * along with the nettle library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
  * MA 02111-1307, USA.
  */
--- des.c
+++ des.c
@@ -14,13 +14,13 @@
  * the Free Software Foundation; either version 2.1 of the License, or (at your
  * option) any later version.
  * 
- * The GNU MP Library is distributed in the hope that it will be useful, but
+ * The nettle library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
  * License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+ * along with the nettle library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
  * MA 02111-1307, USA.
  */
