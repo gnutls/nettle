10276f8a3f90da78ca1de7ec3be05dfc85d29a13
Niels Möller <nisse@lysator.liu.se>
2001-09-06 23:45:44 +0200

    * cbc.c, cbc.h: New files, for general CBC encryption.
    Rev: src/nettle/cbc.c:1.1
    Rev: src/nettle/cbc.h:1.1

---
+++ cbc.c
@@ -0,0 +1,78 @@
+/* cbc.c
+ *
+ * Cipher block chaining mode.
+ */
+
+/* nettle, low-level cryptographics library
+ *
+ * Copyright (C) 2001 Niels Möller
+ *  
+ * The nettle library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * The nettle library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the nettle library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#include "cbc.h"
+
+#include <assert.h>
+
+void
+cbc_encrypt(void *ctx, void (*f)(void *ctx,
+				 unsigned length, uint8_t *dst,
+				 const uint8_t *src),
+	    unsigned block_size, uint8_t *iv,
+	    unsigned length, uint8_t *dst,
+	    const uint8_t *src)
+{
+  assert(!(length % block_size));
+
+  for ( ; length; length -= block_size, src += block_size, dst += block_size)
+    {
+      memxor(iv, src, block_size);
+      f(ctx, dst, src, block_size);
+      memcpy(iv, dst, block_size);
+    }
+}
+
+void
+cbc_decrypt(void *ctx, void (*f)(void *ctx,
+				 unsigned length, uint8_t *dst,
+				 const uint8_t *src),
+	    unsigned block_size, uint8_t *iv,
+	    unsigned length, uint8_t *dst,
+	    const uint8_t *src)
+{
+  assert(!(length % block_size));
+
+  if (!length)
+    return;
+
+  if (src == dst)
+    {
+      /* Keep a copy of the ciphertext. */
+      /* FIXME: If length is large enough, allocate a smaller buffer
+       * and process one buffer size at a time */
+      uint8_t *tmp = alloca(length);
+      memcpy(tmp, src, length);
+      src = tmp;
+    }
+
+  /* Decrypt in ECB mode */
+  f(ctx, dst, src, length);
+
+  /* XOR the cryptotext, shifted one block */
+  memxor(dst, iv, block_size);
+  memxor(dst + block_size, src, length - block_size);
+  memcpy(iv, src + length - block_size, block_size);
+}
---
+++ cbc.h
@@ -0,0 +1,49 @@
+/* cbc.h
+ *
+ * Cipher block chaining mode.
+ */
+
+/* nettle, low-level cryptographics library
+ *
+ * Copyright (C) 2001 Niels Möller
+ *  
+ * The nettle library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * The nettle library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the nettle library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#ifndef NETTLE_CBC_H_INCLUDED
+#define NETTLE_CBC_H_INCLUDED
+
+#include <inttypes.h>
+
+/* Uses a void * for cipher contexts. It's hard to be type safe. */
+
+void
+cbc_encrypt(void *ctx, void (*f)(void *ctx,
+				 unsigned length, uint8_t *dst,
+				 const uint8_t *src),
+	    unsigned block_size, uint8_t *iv,
+	    unsigned length, uint8_t *dst,
+	    const uint8_t *src);
+
+void
+cbc_decrypt(void *ctx, void (*f)(void *ctx,
+				 unsigned length, uint8_t *dst,
+				 const uint8_t *src),
+	    unsigned block_size, uint8_t *iv,
+	    unsigned length, uint8_t *dst,
+	    const uint8_t *src);
+
+#endif /* NETTLE_CBC_H_INCLUDED */
