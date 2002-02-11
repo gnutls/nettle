bf27cdc459fca584d04f3aa7c7f35d4321f5ac0c
Niels Möller <nisse@lysator.liu.se>
2002-02-11 18:51:50 +0100

    * aes-set-key.c, aes-tables.c: New files, split off from aes.c. Tables are now not static, but use a _aes_ prefix on their names.
    * aes-internal.h: New file.
    
    Rev: src/nettle/aes-internal.h:1.1
    Rev: src/nettle/aes-set-key.c:1.1
    Rev: src/nettle/aes-tables.c:1.1

---
+++ aes-internal.h
@@ -0,0 +1,51 @@
+/* aes-internal.h
+ *
+ * The aes/rijndael block cipher.
+ */
+
+/* nettle, low-level cryptographics library
+ *
+ * Copyright (C) 2001 Niels Möller
+ *  
+ * The nettle library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * The nettle library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the nettle library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#ifndef NETTLE_AES_INTERNAL_H_INCLUDED
+#define NETTLE_AES_INTERNAL_H_INCLUDED
+
+#include "aes.h"
+
+/* Macros */
+#define ROTBYTE(x) (((x) >> 8) | (((x) & 0xff) << 24))
+#define ROTRBYTE(x) (((x) << 8) | (((x) >> 24) & 0xff))
+#define SUBBYTE(x, box) (((box)[((x) & 0xff)]) | \
+                        ((box)[(((x) >> 8) & 0xff)] << 8) | \
+                        ((box)[(((x) >> 16) & 0xff)] << 16) | \
+                        ((box)[(((x) >> 24) & 0xff)] << 24))
+
+/* Don't pollute global namespace too much */
+#define dtbl _aes_dtbl
+#define itbl _aes_itbl
+#define sbox _aes_sbox
+#define isbox _aes_isbox
+
+/* Internal tables */
+extern const uint32_t dtbl[];
+extern const uint32_t itbl[];
+extern const uint8_t sbox[256];
+extern const uint8_t isbox[256];
+
+#endif /* NETTLE_AES_INTERNAL_H_INCLUDED */
