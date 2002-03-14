167822ccaab8cda62dccd61fc65d9835c5e2fd78
Niels Möller <nisse@lysator.liu.se>
2002-03-14 13:55:25 +0100

    Added copyright notice.
    Rev: src/nettle/sparc/aes.asm:1.67
    Rev: src/nettle/x86/aes.asm:1.2

--- sparc/aes.asm
+++ sparc/aes.asm
@@ -1,4 +1,23 @@
 ! -*- mode: asm; asm-comment-char: ?!; -*-  
+! nettle, low-level cryptographics library
+! 
+! Copyright (C) 2002 Niels Möller
+!  
+! The nettle library is free software; you can redistribute it and/or modify
+! it under the terms of the GNU Lesser General Public License as published by
+! the Free Software Foundation; either version 2.1 of the License, or (at your
+! option) any later version.
+! 
+! The nettle library is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+! or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+! License for more details.
+! 
+! You should have received a copy of the GNU Lesser General Public License
+! along with the nettle library; see the file COPYING.LIB.  If not, write to
+! the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+! MA 02111-1307, USA.
+
 	! Used registers:	%l0,1,2,3,4,5,6,7
 	!			%i0,1,2,3,4 (%i6=%fp, %i7 = return)
 	!			%o0,1,2,3 (%o6=%sp)
--- x86/aes.asm
+++ x86/aes.asm
@@ -1,3 +1,22 @@
+! nettle, low-level cryptographics library
+! 
+! Copyright (C) 2001, 2002 Rafael R. Sevilla
+!  
+! The nettle library is free software; you can redistribute it and/or modify
+! it under the terms of the GNU Lesser General Public License as published by
+! the Free Software Foundation; either version 2.1 of the License, or (at your
+! option) any later version.
+! 
+! The nettle library is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+! or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+! License for more details.
+! 
+! You should have received a copy of the GNU Lesser General Public License
+! along with the nettle library; see the file COPYING.LIB.  If not, write to
+! the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+! MA 02111-1307, USA.
+
 	.file	"rijndael.s"
 
 	.data
