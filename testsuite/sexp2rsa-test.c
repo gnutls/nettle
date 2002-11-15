7f57807bc5f0496d8955e59da04bb0f1ae071fae
Niels Möller <nisse@lysator.liu.se>
2002-11-15 09:15:03 +0100

    (test_main): Added a leading zero in the private key expression.
    Rev: src/nettle/testsuite/sexp2rsa-test.c:1.4

--- testsuite/sexp2rsa-test.c
+++ testsuite/sexp2rsa-test.c
@@ -35,9 +35,9 @@ test_main(void)
 	     "6a8944f0b92928313a6233323a014875"
 	     "1e622d6d58e3bb094afd6edacf737035"
 	     "1d068e2ce9f565c5528c4a7473292831"
-	     "3a6333313af8a458ea73a018dc6fa568"
-	     "63e3bc6de405f364f77dee6f0962679e"
-	     "a1a8282e292929")));
+	     "3a6333323a00f8a458ea73a018dc6fa5"
+	     "6863e3bc6de405f364f77dee6f096267"
+	     "9ea1a8282e292929")));
 
   test_rsa_key(&pub, &priv);
   
