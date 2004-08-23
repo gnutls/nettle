d1404928f89083f9cb136c1ee523adc44a150394
Niels Möller <nisse@lysator.liu.se>
2004-08-23 19:05:17 +0200

    (test_main): Added first half of a collision test case.
    Rev: src/nettle/testsuite/md5-test.c:1.3

--- testsuite/md5-test.c
+++ testsuite/md5-test.c
@@ -29,6 +29,14 @@ test_main(void)
 	    "1234567890123456789012345678901234567890"
 	    "1234567890123456789012345678901234567890",
 	    H("57EDF4A22BE3C955 AC49DA2E2107B67A"));
+#if 0
+  /* Collisions, reported by Xiaoyun Wang1, Dengguo Feng2, Xuejia
+     Lai3, Hongbo Yu1, http://eprint.iacr.org/2004/199 */
 
+  test_hash(&nettle_md5,
+	    HL("d131dd02 c5e6eec4 693d9a06 98aff95c 2fcab587 12467eab 4004583e b8fb7f89"
+	       "55ad3406 09f4b302 83e48883 2571415a 085125e8 f7cdc99f d91dbdf2 80373c5b"),
+	    H("1f160396 efc71ff4 bcff659f bf9d0fa3"));
+#endif
   SUCCESS();
 }
