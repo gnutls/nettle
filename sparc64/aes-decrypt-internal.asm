dacff0c5e5d32a4d451613778eb5981d1095f0fd
Niels Möller <nisse@lysator.liu.se>
2005-10-16 22:35:39 +0200

    * sparc64/aes-encrypt-internal.asm: Include sparc/aes.m4. * sparc64/aes-decrypt-internal.asm: Likewise.
    Rev: src/nettle/sparc64/aes-decrypt-internal.asm:1.2

--- sparc64/aes-decrypt-internal.asm
+++ sparc64/aes-decrypt-internal.asm
@@ -22,6 +22,10 @@ C The only difference between this code and the sparc32 code is the
 C frame offsets, and the magic BIAS when accessing the stack (which
 C doesn't matter, since we don't access any data on the stack).
 
+
+C Use the same AES macros as on sparc32.
+include_src(sparc/aes.m4)
+
 C	Arguments
 define(<CTX>,	<%i0>)
 define(<T>,	<%i1>)
