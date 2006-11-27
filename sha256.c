69ac8060474ce79dba40ab34f4463d18eabbf4aa
Niels Möller <nisse@lysator.liu.se>
2006-11-27 23:34:44 +0100

    * sha256.c: Changed copyright notice to use the LGPL.
    Rev: src/nettle/sha256.c:1.5

--- sha256.c
+++ sha256.c
@@ -9,10 +9,10 @@
  *
  * Copyright (C) 2001 Niels Möller
  *  
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * The nettle library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
  * The nettle library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
