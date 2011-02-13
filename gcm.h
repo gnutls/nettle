ccba0ebe59d083f04e865375aaf2ff441561fcc1
Niels Möller <nisse@lysator.liu.se>
2011-02-13 15:17:22 +0100

    (GCM_TABLE_BITS): Increase table size to 8 bits, corresponding to 4 KByte of key-dependent tables.
    Rev: nettle/gcm.h:1.10

--- gcm.h
+++ gcm.h
@@ -58,7 +58,7 @@ extern "C" {
 #define GCM_BLOCK_SIZE 16
 #define GCM_IV_SIZE (GCM_BLOCK_SIZE - 4)
 
-#define GCM_TABLE_BITS 4
+#define GCM_TABLE_BITS 8
 
 /* To make sure that we have proper alignment. */
 union gcm_block
