0134b52fb65700dde6245d806297a43b33b05198
Niels Möller <nisse@lysator.liu.se>
2011-06-20 12:59:36 +0200

    (test_main): Added some more multiple block tests.
    Rev: nettle/testsuite/serpent-test.c:1.8

--- testsuite/serpent-test.c
+++ testsuite/serpent-test.c
@@ -184,12 +184,14 @@ test_main(void)
 	      HL("e87450aa0fd87293fd0371483a459bd2"),
 	      HL("a78a7a8d392f629d bd13674c8dce6fa2"),
 	      H("b3d488986c80dea7 c5ebdab4907871c9"));
+
   test_cipher(&nettle_serpent128,
 	      HL("e87450aa0fd87293fd0371483a459bd2"),
 	      HL("a78a7a8d392f629d bd13674c8dce6fa2"
 		 "930c74dec02a11d8 c80d90b5e5c887a7"),
 	      H("b3d488986c80dea7 c5ebdab4907871c9"
 		"a4b92b13b79afb37 5518b01bfd706a37"));
+
   test_cipher(&nettle_serpent128,
 	      HL("e87450aa0fd87293fd0371483a459bd2"),
 	      HL("a78a7a8d392f629d bd13674c8dce6fa2"
@@ -198,6 +200,18 @@ test_main(void)
 	      H("b3d488986c80dea7 c5ebdab4907871c9"
 		"a4b92b13b79afb37 5518b01bfd706a37"
 		"8e44c2d463df4531 165461699edbad03"));
+
+  test_cipher(&nettle_serpent128,
+	      HL("91c8e949e12f0e38 7b2473238a3df1b6"),
+	      HL("00000000 00000001 00000002 00000003"
+		 "00000004 00000005 00000006 00000007"
+		 "00000008 00000009 0000000a 0000000b"
+		 "0000000c 0000000d 0000000e 0000000f"),
+	      H("2db9f0a39d4f31a4 b1a83cd1032fe1bd"
+		"3606caa84a220b1b f6f43ff80a831203"
+		"8c6c8d2793dc10b3 904d30e194f086a6"
+		"b2f3e932b9b3f8d1 d4d074f7bd1ff7a3"));
+	      
   test_cipher(&nettle_serpent128,
 	      HL("e87450aa0fd87293fd0371483a459bd2"),
 	      HL("a78a7a8d392f629d bd13674c8dce6fa2"
@@ -208,6 +222,70 @@ test_main(void)
 		"a4b92b13b79afb37 5518b01bfd706a37"
 		"8e44c2d463df4531 165461699edbad03"
 		"30ac8c52697102ae 3b725dba79ceb250"));
-  
+
+  test_cipher(&nettle_serpent128,
+	      HL("e87450aa0fd87293fd0371483a459bd2"),
+	      HL("a78a7a8d392f629d bd13674c8dce6fa2"
+		 "930c74dec02a11d8 c80d90b5e5c887a7"
+		 "83c92a921b5b2028 d9cb313a5f07ab09"
+		 "672eadf1624a2ed0 c42d1b08b076f75a"
+		 "7378272aa57ad7c8 803e326689541266"),
+	      H("b3d488986c80dea7 c5ebdab4907871c9"
+		"a4b92b13b79afb37 5518b01bfd706a37"
+		"8e44c2d463df4531 165461699edbad03"
+		"30ac8c52697102ae 3b725dba79ceb250"
+		"d308b83478e86dbb 629f18736cca042f"));
+
+  test_cipher(&nettle_serpent128,
+	      HL("e87450aa0fd87293fd0371483a459bd2"),
+	      HL("a78a7a8d392f629d bd13674c8dce6fa2"
+		 "930c74dec02a11d8 c80d90b5e5c887a7"
+		 "83c92a921b5b2028 d9cb313a5f07ab09"
+		 "672eadf1624a2ed0 c42d1b08b076f75a"
+		 "7378272aa57ad7c8 803e326689541266"
+		 "b7a2efda5721776f 4113d63a702ac3ae"),
+	      H("b3d488986c80dea7 c5ebdab4907871c9"
+		"a4b92b13b79afb37 5518b01bfd706a37"
+		"8e44c2d463df4531 165461699edbad03"
+		"30ac8c52697102ae 3b725dba79ceb250"
+		"d308b83478e86dbb 629f18736cca042f"
+		"006b89e494469adf 0ee78c60684dff86"));
+    
+  test_cipher(&nettle_serpent128,
+	      HL("e87450aa0fd87293fd0371483a459bd2"),
+	      HL("a78a7a8d392f629d bd13674c8dce6fa2"
+		 "930c74dec02a11d8 c80d90b5e5c887a7"
+		 "83c92a921b5b2028 d9cb313a5f07ab09"
+		 "672eadf1624a2ed0 c42d1b08b076f75a"
+		 "7378272aa57ad7c8 803e326689541266"
+		 "b7a2efda5721776f 4113d63a702ac3ae"
+		 "cd1be7bbfad74819 644617f8656e9e5b"),
+	      H("b3d488986c80dea7 c5ebdab4907871c9"
+		"a4b92b13b79afb37 5518b01bfd706a37"
+		"8e44c2d463df4531 165461699edbad03"
+		"30ac8c52697102ae 3b725dba79ceb250"
+		"d308b83478e86dbb 629f18736cca042f"
+		"006b89e494469adf 0ee78c60684dff86"
+		"5f2c99908ee77ffe aea3d30cb78a1ce1"));
+
+  test_cipher(&nettle_serpent128,
+	      HL("e87450aa0fd87293fd0371483a459bd2"),
+	      HL("a78a7a8d392f629d bd13674c8dce6fa2"
+		 "930c74dec02a11d8 c80d90b5e5c887a7"
+		 "83c92a921b5b2028 d9cb313a5f07ab09"
+		 "672eadf1624a2ed0 c42d1b08b076f75a"
+		 "7378272aa57ad7c8 803e326689541266"
+		 "b7a2efda5721776f 4113d63a702ac3ae"
+		 "cd1be7bbfad74819 644617f8656e9e5b"
+		 "34d449409c1f850a 4cb6700d6ef3405f"),
+	      H("b3d488986c80dea7 c5ebdab4907871c9"
+		"a4b92b13b79afb37 5518b01bfd706a37"
+		"8e44c2d463df4531 165461699edbad03"
+		"30ac8c52697102ae 3b725dba79ceb250"
+		"d308b83478e86dbb 629f18736cca042f"
+		"006b89e494469adf 0ee78c60684dff86"
+		"5f2c99908ee77ffe aea3d30cb78a1ce1"
+		"ebe855dd51532477 4d2d55969e032e6c"));
+
   SUCCESS();
 }
