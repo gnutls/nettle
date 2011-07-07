0a2aed43c74effdc10e17c50d4070afd71894ca3
Niels Möller <nisse@lysator.liu.se>
2011-07-07 22:42:55 +0200

    (read_file): Bug fix, in dependence on initial size on max_size.
    Rev: nettle/examples/io.c:1.4

--- examples/io.c
+++ examples/io.c
@@ -88,8 +88,13 @@ read_file(const char *name, unsigned max_size, char **contents)
     size = max_size;
   else
     size = 100;
-  
-  for (size = 100, done = 0;
+
+  /* FIXME: The use of feof and ferror in this loop is a bit confused
+     (but I think it is still correct). We should check the return
+     value of fread, and call feof and/or ferror when we get a short
+     item count. */	
+
+  for (done = 0;
        (!max_size || done < max_size) && !feof(f);
        size *= 2)
     {
