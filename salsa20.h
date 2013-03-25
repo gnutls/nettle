a3376b3ad15c8a0972671fb2ea570b9016659e7b
Niels Möller <nisse@lysator.liu.se>
2013-03-25 15:08:38 +0100

    Whitespace cleanup.

--- salsa20.h
+++ salsa20.h
@@ -7,23 +7,23 @@
  *
  * Copyright (C) 2012 Simon Josefsson
  * Copyright (C) 2001 Niels Möller
- *  
+ *
  * The nettle library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or (at your
  * option) any later version.
- * 
+ *
  * The nettle library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the nettle library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02111-1301, USA.
  */
- 
+
 #ifndef NETTLE_SALSA20_H_INCLUDED
 #define NETTLE_SALSA20_H_INCLUDED
 
@@ -80,8 +80,8 @@ salsa20_crypt(struct salsa20_ctx *ctx,
 
 void
 salsa20r12_crypt(struct salsa20_ctx *ctx,
-	      unsigned length, uint8_t *dst,
-	      const uint8_t *src);
+		 unsigned length, uint8_t *dst,
+		 const uint8_t *src);
 
 void
 _salsa20_core(uint32_t *dst, const uint32_t *src, unsigned rounds);
