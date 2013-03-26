6f377753a73b01984ee5bda17f532874c74a735a
Niels Möller <nisse@lysator.liu.se>
2013-03-26 14:13:17 +0100

    Updated FIPS reference.

--- sha512.c
+++ sha512.c
@@ -1,8 +1,8 @@
 /* sha512.c
  *
- * The sha512 hash function FIXME: Add the SHA384 variant.
+ * The sha512 hash function.
  *
- * See http://csrc.nist.gov/publications/fips/fips180-2/fips180-2.pdf
+ * See http://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf
  */
 
 /* nettle, low-level cryptographics library
