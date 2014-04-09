7aef18d3e4cbdbef1c24309475ae10f66e4456ff
Niels Möller <nisse@lysator.liu.se>
2014-04-09 14:53:20 +0200

    nettle_hash instances for sha512_224 and sha512_256.

---
+++ sha512-256-meta.c
@@ -0,0 +1,40 @@
+/* sha512-256-meta.c */
+
+/* nettle, low-level cryptographics library
+ *
+ * Copyright (C) 2014 Niels Möller
+ *  
+ * The nettle library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * The nettle library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the nettle library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02111-1301, USA.
+ */
+
+#if HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "nettle-meta.h"
+
+#include "sha2.h"
+
+const struct nettle_hash nettle_sha512_256 =
+  {
+    "sha512-256", sizeof(struct sha512_ctx),
+    SHA512_256_DIGEST_SIZE,
+    SHA512_256_DATA_SIZE,
+    (nettle_hash_init_func *) sha512_256_init,
+    (nettle_hash_update_func *) sha512_256_update,
+    (nettle_hash_digest_func *) sha512_256_digest
+  };
+
