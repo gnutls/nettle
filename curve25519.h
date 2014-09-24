82a74eda4503c1c7e6b4a0a2cb881eabfa2417c8
Niels Möller <nisse@lysator.liu.se>
2014-09-24 20:42:18 +0200

    Fixed extern "C" block.

--- curve25519.h
+++ curve25519.h
@@ -51,4 +51,8 @@ curve25519_mul_g (uint8_t *q, const uint8_t *n);
 int
 curve25519_mul (uint8_t *q, const uint8_t *n, const uint8_t *p);
 
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* NETTLE_CURVE25519_H */
