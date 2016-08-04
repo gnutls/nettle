52b9223126b3f997c00d399166c006ae28669068
Niels Möller <nisse@lysator.liu.se>
2016-08-04 10:18:23 +0200

    Reject invalid keys, with even moduli, in rsa_compute_root_tr.

--- rsa-sign-tr.c
+++ rsa-sign-tr.c
@@ -88,6 +88,14 @@ rsa_compute_root_tr(const struct rsa_public_key *pub,
   int res;
   mpz_t t, mb, xb, ri;
 
+  /* mpz_powm_sec handles only odd moduli. If p, q or n is even, the
+     key is invalid and rejected by rsa_private_key_prepare. However,
+     some applications, notably gnutls, don't use this function, and
+     we don't want an invalid key to lead to a crash down inside
+     mpz_powm_sec. So do an additional check here. */
+  if (mpz_even_p (pub->n) || mpz_even_p (key->p) || mpz_even_p (key->q))
+    return 0;
+
   mpz_init (mb);
   mpz_init (xb);
   mpz_init (ri);
