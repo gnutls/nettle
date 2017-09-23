f773a9dd570de9a9e1f357b552a602594975ca02
Niels Möller <nisse@lysator.liu.se>
2017-09-23 10:54:50 +0200

    Reduce duplication in ecc-add-test.
    * testsuite/ecc-add-test.c (test_main): Reduce test duplication.
    Use ecc->add_hhh_itch.

--- testsuite/ecc-add-test.c
+++ testsuite/ecc-add-test.c
@@ -13,19 +13,23 @@ test_main (void)
       mp_limb_t *g2 = xalloc_limbs (ecc_size_j (ecc));
       mp_limb_t *g3 = xalloc_limbs (ecc_size_j (ecc));
       mp_limb_t *p = xalloc_limbs (ecc_size_j (ecc));
-      mp_limb_t *scratch = xalloc_limbs (ECC_ADD_JJJ_ITCH(ecc->p.size));
+      mp_limb_t *scratch = xalloc_limbs (ecc->add_hhh_itch);
+
+      ASSERT (ecc->dup_itch <= ecc->add_hhh_itch);
+
+      ecc_a_to_j (ecc, g, ecc->g);
 
       if (ecc->p.bit_size == 255)
 	{
 	  mp_limb_t *z = xalloc_limbs (ecc_size_j (ecc));
+
+	  ASSERT (ecc->add_hh == ecc_add_eh);
+	  ASSERT (ecc->add_hhh == ecc_add_ehh);
+	  ASSERT (ecc->add_hh_itch <= ecc->add_hhh_itch);
+
 	  /* Zero point has x = 0, y = 1, z = 1 */
 	  mpn_zero (z, 3*ecc->p.size);
 	  z[ecc->p.size] = z[2*ecc->p.size] = 1;
-
-	  assert (ecc->add_hh == ecc_add_eh);
-	  assert (ecc->add_hhh == ecc_add_ehh);
-
-	  ecc_a_to_j (ecc, g, ecc->g);
 
 	  ecc->add_hhh (ecc, p, z, z, scratch);
 	  test_ecc_mul_h (i, 0, p);
@@ -45,48 +49,32 @@ test_main (void)
 	  ecc->add_hh (ecc, g2, g, g, scratch);
 	  test_ecc_mul_h (i, 2, g2);
 
-	  ecc->add_hhh (ecc, g3, g, g2, scratch);
-	  test_ecc_mul_h (i, 3, g3);
-
-	  ecc->add_hh (ecc, g3, g2, g, scratch);
-	  test_ecc_mul_h (i, 3, g3);
-
-	  ecc->add_hhh (ecc, p, g, g3, scratch);
-	  test_ecc_mul_h (i, 4, p);
-
-	  ecc->add_hh (ecc, p, g3, g, scratch);
-	  test_ecc_mul_h (i, 4, p);
-
-	  ecc->add_hhh (ecc, p, g2, g2, scratch);
-	  test_ecc_mul_h (i, 4, p);
-
 	  free (z);
 	}
       else
 	{
-	  assert (ecc->add_hhh == ecc_add_jjj);
-	  assert (ecc->dup == ecc_dup_jj);
-
-	  ecc_a_to_j (ecc, g, ecc->g);
-
-	  ecc->dup (ecc, g2, g, scratch);
-	  test_ecc_mul_h (i, 2, g2);
-
-	  ecc->add_hhh (ecc, g3, g, g2, scratch);
-	  test_ecc_mul_h (i, 3, g3);
-
-	  ecc->add_hhh (ecc, g3, g2, g, scratch);
-	  test_ecc_mul_h (i, 3, g3);
-
-	  ecc->add_hhh (ecc, p, g, g3, scratch);
-	  test_ecc_mul_h (i, 4, p);
-
-	  ecc->add_hhh (ecc, p, g3, g, scratch);
-	  test_ecc_mul_h (i, 4, p);
-
-	  ecc->dup (ecc, p, g2, scratch);
-	  test_ecc_mul_h (i, 4, p);
+	  ASSERT (ecc->add_hhh == ecc_add_jjj);
+	  ASSERT (ecc->dup == ecc_dup_jj);
 	}
+
+      ecc->dup (ecc, g2, g, scratch);
+      test_ecc_mul_h (i, 2, g2);
+
+      ecc->add_hhh (ecc, g3, g, g2, scratch);
+      test_ecc_mul_h (i, 3, g3);
+
+      ecc->add_hhh (ecc, g3, g2, g, scratch);
+      test_ecc_mul_h (i, 3, g3);
+
+      ecc->add_hhh (ecc, p, g, g3, scratch);
+      test_ecc_mul_h (i, 4, p);
+
+      ecc->add_hhh (ecc, p, g3, g, scratch);
+      test_ecc_mul_h (i, 4, p);
+
+      ecc->dup (ecc, p, g2, scratch);
+      test_ecc_mul_h (i, 4, p);
+
       free (g);
       free (g2);
       free (g3);
