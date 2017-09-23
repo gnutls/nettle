49181d3f9df4ab35d3372d7e7ca6388c282c8101
Niels Möller <nisse@lysator.liu.se>
2017-09-23 11:01:39 +0200

    Reduce duplication in ecc-dup-test.
    * testsuite/ecc-dup-test.c (test_main): Reduce test duplication.
    Use ecc->dup_itch.

--- testsuite/ecc-dup-test.c
+++ testsuite/ecc-dup-test.c
@@ -10,37 +10,34 @@ test_main (void)
       const struct ecc_curve *ecc = ecc_curves[i];
       mp_limb_t *g = xalloc_limbs (ecc_size_j (ecc));
       mp_limb_t *p = xalloc_limbs (ecc_size_j (ecc));
-      mp_limb_t *scratch = xalloc_limbs (ECC_DUP_EH_ITCH(ecc->p.size));;
+      mp_limb_t *scratch = xalloc_limbs (ecc->dup_itch);
+
+      ecc_a_to_j (ecc, g, ecc->g);
 
       if (ecc->p.bit_size == 255)
 	{
 	  mp_limb_t *z = xalloc_limbs (ecc_size_j (ecc));
+
+	  ASSERT (ecc->dup == ecc_dup_eh);
+
 	  /* Zero point has x = 0, y = 1, z = 1 */
 	  mpn_zero (z, 3*ecc->p.size);
 	  z[ecc->p.size] = z[2*ecc->p.size] = 1;
 	  
-	  ecc_a_to_j (ecc, g, ecc->g);
-
 	  ecc->dup (ecc, p, z, scratch);
 	  test_ecc_mul_h (i, 0, p);
 
-	  ecc->dup (ecc, p, g, scratch);
-	  test_ecc_mul_h (i, 2, p);
-
-	  ecc->dup (ecc, p, p, scratch);
-	  test_ecc_mul_h (i, 4, p);
 	  free (z);
 	}
       else
-	{
-	  ecc_a_to_j (ecc, g, ecc->g);
+	ASSERT (ecc->dup == ecc_dup_jj);
 
-	  ecc->dup (ecc, p, g, scratch);
-	  test_ecc_mul_h (i, 2, p);
+      ecc->dup (ecc, p, g, scratch);
+      test_ecc_mul_h (i, 2, p);
 
-	  ecc->dup (ecc, p, p, scratch);
-	  test_ecc_mul_h (i, 4, p);
-	}
+      ecc->dup (ecc, p, p, scratch);
+      test_ecc_mul_h (i, 4, p);
+
       free (p);
       free (g);
       free (scratch);
