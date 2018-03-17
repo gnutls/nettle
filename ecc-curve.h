8bf4747d9cca7fd2cdd6ca8e14bce7cb334b2a7a
Niels Möller <nisse@lysator.liu.se>
2018-03-17 17:25:03 +0100

    Add leading underscore to data symbols.

--- ecc-curve.h
+++ ecc-curve.h
@@ -41,14 +41,11 @@ extern "C" {
 /* The contents of this struct is internal. */
 struct ecc_curve;
 
-/* FIXME: Rename with leading underscore. Due to ABI subtleties,
-   applications should not refer to these directly, but use the below
-   accessor functions. */
-extern const struct ecc_curve nettle_secp_192r1;
-extern const struct ecc_curve nettle_secp_224r1;
-extern const struct ecc_curve nettle_secp_256r1;
-extern const struct ecc_curve nettle_secp_384r1;
-extern const struct ecc_curve nettle_secp_521r1;
+extern const struct ecc_curve _nettle_secp_192r1;
+extern const struct ecc_curve _nettle_secp_224r1;
+extern const struct ecc_curve _nettle_secp_256r1;
+extern const struct ecc_curve _nettle_secp_384r1;
+extern const struct ecc_curve _nettle_secp_521r1;
 
 #ifdef __GNUC__
 #define NETTLE_PURE __attribute__((pure))
