21c9be59b5bfb0b03f6b701aa275ac3bc764660e
Niels Möller <nisse@lysator.liu.se>
2020-01-02 07:45:33 +0100

    Fix comment typo.

--- ecc-internal.h
+++ ecc-internal.h
@@ -154,7 +154,7 @@ struct ecc_modulo
   /* B^size mod m. Expected to have at least 32 leading zeros
      (equality for secp_256r1). */
   const mp_limb_t *B;
-  /* 2^{bit_size} - p, same value as above, but shifted. */
+  /* 2^{bit_size} - m, same value as above, but shifted. */
   const mp_limb_t *B_shifted;
   /* m +/- 1, for redc, excluding redc_size low limbs. */
   const mp_limb_t *redc_mpm1;
