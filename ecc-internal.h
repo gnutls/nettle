f1bbbcae39e99b127aaf063ed0bd80799155aa4d
Niels Möller <nisse@lysator.liu.se>
2020-11-08 11:38:13 +0100

    Reduce scratch need for ecc_add_jja

--- ecc-internal.h
+++ ecc-internal.h
@@ -447,7 +447,7 @@ curve448_eh_to_x (mp_limb_t *xp, const mp_limb_t *p,
 #define ECC_DUP_JJ_ITCH(size) (4*(size))
 #define ECC_DUP_EH_ITCH(size) (3*(size))
 #define ECC_DUP_TH_ITCH(size) (3*(size))
-#define ECC_ADD_JJA_ITCH(size) (6*(size))
+#define ECC_ADD_JJA_ITCH(size) (5*(size))
 #define ECC_ADD_JJJ_ITCH(size) (6*(size))
 #define ECC_ADD_EH_ITCH(size) (4*(size))
 #define ECC_ADD_EHH_ITCH(size) (4*(size))
