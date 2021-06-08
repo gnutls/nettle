cd6059aebdd3059fbcf674dddb850b821c13b6c2
Niels Möller <nisse@lysator.liu.se>
2021-06-08 21:31:39 +0200

    Change _rsa_sec_compute_root_tr to take a fix input size.
    Improves consistency with _rsa_sec_compute_root, and fixes zero-input bug.
    
    (cherry picked from commit 485b5e2820a057e873b1ba812fdb39cae4adf98c)

--- rsa-sec-decrypt.c
+++ rsa-sec-decrypt.c
@@ -58,9 +58,12 @@ rsa_sec_decrypt(const struct rsa_public_key *pub,
   TMP_GMP_ALLOC (m, mpz_size(pub->n));
   TMP_GMP_ALLOC (em, key->size);
 
-  res = _rsa_sec_compute_root_tr (pub, key, random_ctx, random, m,
-				  mpz_limbs_read(gibberish),
-				  mpz_size(gibberish));
+  /* We need a copy because m can be shorter than key_size,
+   * but _rsa_sec_compute_root_tr expect all inputs to be
+   * normalized to a key_size long buffer length */
+  mpz_limbs_copy(m, gibberish, mpz_size(pub->n));
+
+  res = _rsa_sec_compute_root_tr (pub, key, random_ctx, random, m, m);
 
   mpn_get_base256 (em, key->size, m, mpz_size(pub->n));
 
