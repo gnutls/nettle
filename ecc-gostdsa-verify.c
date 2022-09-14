4eb5868ce8963827aa6e00a01ed90df488b288fe
Niels Möller <nisse@lysator.liu.se>
2022-09-14 16:17:52 +0200

    Fix ECDSA verify corner case
    * ecc-ecdsa-verify.c (ecc_ecdsa_verify): Use ecc_nonsec_add_jjj,
    to produce correct result in a corner case where point addition
    needs to use point duplication. Also use ecc_j_to_a rather than
    ecc->h_to_a, since ecdsa supports only weierstrass curves.
    * ecc-gostdsa-verify.c (ecc_gostdsa_verify): Analogous change.
    
    * testsuite/ecdsa-verify-test.c (test_main): Add corresponding test.
    * testsuite/ecdsa-sign-test.c (test_main): And a test producing
    the problematic signature.

--- ecc-gostdsa-verify.c
+++ ecc-gostdsa-verify.c
@@ -115,10 +115,11 @@ ecc_gostdsa_verify (const struct ecc_curve *ecc,
   ecc->mul_g (ecc, P1, z1, P1 + 3*ecc->p.size);
 
   /* Total storage: 6*ecc->p.size + ecc->add_hhh_itch */
-  ecc->add_hhh (ecc, P1, P1, P2, P1 + 3*ecc->p.size);
+  if (!ecc_nonsec_add_jjj (ecc, P1, P1, P2, P1 + 3*ecc->p.size))
+    return 0;
 
   /* x coordinate only, modulo q */
-  ecc->h_to_a (ecc, 2, P2, P1, P1 + 3*ecc->p.size);
+  ecc_j_to_a (ecc, 2, P2, P1, P1 + 3*ecc->p.size);
 
   return (mpn_cmp (rp, P2, ecc->p.size) == 0);
 #undef P2
