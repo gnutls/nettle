d67fdaf6cecb16a9d2e093770488052f1d0944aa
Niels Möller <nisse@lysator.liu.se>
2023-01-25 10:56:10 +0100

    Add ocb tests with larger messages.

--- testsuite/ocb-test.c
+++ testsuite/ocb-test.c
@@ -144,4 +144,71 @@ test_main(void)
 		 "A5DDBFC5787E50B5CC55EE507BCB084E"), /* ciphertext */
 	    SHEX("BBAA9988776655443322110F"), /* nonce */
 	    SHEX("479AD363AC366B95 A98CA5F3000B1479")); /* tag */
+
+  /* 16 blocks, not verified with other implementations or any
+     authoritative test vector.not an authoritative test vector. */
+  test_aead(&nettle_ocb_aes128, NULL,
+	    SHEX("000102030405060708090A0B0C0D0E0F"), /* key */
+	    SHEX(""), /* auth data */
+	    SHEX("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+		 "202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f"
+		 "404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f"
+		 "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f"
+		 "808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f"
+		 "a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebf"
+		 "c0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf"
+		 "e0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"),
+	    SHEX("4412923493c57d5d e0d700f753cce0d1"
+		 "d2d95060122e9f15 a5ddbfc5787e50b5"
+		 "11dfb888da244711 f051dbce82b0b9a7"
+		 "cb14869b164e55eb 578e41fa435ff220"
+		 "25ed114f6ec18cd6 7b743ab299e596f6"
+		 "6100fba539db164d 765eaff0bf489ace"
+		 "90ff6af96d1c395b 8dd586b154a0ecea"
+		 "504395c5592cf2f0 03a3878585a0bfd3"
+		 "b4039d15bc47a6d6 4a51f7302a976bb0"
+		 "175167bcb5d8f071 a3faff70544ab2ba"
+		 "52947d35d6e545e9 bda57b3972ecad10"
+		 "f0e85aec389f4276 2e58978918d4c285"
+		 "c2088ca8ac48095c 976065aa47766756"
+		 "7a507bab08315b2e 36327e8103a6a70d"
+		 "7f9f5318684697b2 bf95d65fa5458e6e"
+		 "f40a974cb940e8fd 63baf0ce96773279"),
+	    SHEX("BBAA9988776655443322110F"), /* nonce */
+	    SHEX("3aa4f4e4b4ff142c 9357291589fa25d8")); /* tag */
+
+  /* 16 complete blocks + left-over bytes, not verified with other
+     implementations or any authoritative test vector. */
+  test_aead(&nettle_ocb_aes128, NULL,
+	    SHEX("000102030405060708090A0B0C0D0E0F"), /* key */
+	    SHEX(""), /* auth data */
+	    SHEX("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+		 "202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f"
+		 "404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f"
+		 "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f"
+		 "808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f"
+		 "a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebf"
+		 "c0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf"
+		 "e0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"
+		 "deadbeaf"),
+	    SHEX("4412923493c57d5d e0d700f753cce0d1"
+		 "d2d95060122e9f15 a5ddbfc5787e50b5"
+		 "11dfb888da244711 f051dbce82b0b9a7"
+		 "cb14869b164e55eb 578e41fa435ff220"
+		 "25ed114f6ec18cd6 7b743ab299e596f6"
+		 "6100fba539db164d 765eaff0bf489ace"
+		 "90ff6af96d1c395b 8dd586b154a0ecea"
+		 "504395c5592cf2f0 03a3878585a0bfd3"
+		 "b4039d15bc47a6d6 4a51f7302a976bb0"
+		 "175167bcb5d8f071 a3faff70544ab2ba"
+		 "52947d35d6e545e9 bda57b3972ecad10"
+		 "f0e85aec389f4276 2e58978918d4c285"
+		 "c2088ca8ac48095c 976065aa47766756"
+		 "7a507bab08315b2e 36327e8103a6a70d"
+		 "7f9f5318684697b2 bf95d65fa5458e6e"
+		 "f40a974cb940e8fd 63baf0ce96773279"
+		 "1dd97611"),
+	    SHEX("BBAA9988776655443322110F"), /* nonce */
+	    SHEX("8a24edb596b59425 43ec197d5369979b")); /* tag */
+
 }
