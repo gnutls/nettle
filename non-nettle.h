4ab2163263b4cbaa3a3992461e1449c13ed08788
Niels Möller <nisse@lysator.liu.se>
2023-12-04 21:18:07 +0100

    Delete benchmarking of openssl blowfish, cast128 and des.

--- non-nettle.h
+++ non-nettle.h
@@ -75,9 +75,6 @@ extern void nettle_openssl_init(void);
 extern const struct nettle_cipher nettle_openssl_aes128;
 extern const struct nettle_cipher nettle_openssl_aes192;
 extern const struct nettle_cipher nettle_openssl_aes256;
-extern const struct nettle_cipher nettle_openssl_blowfish128;
-extern const struct nettle_cipher nettle_openssl_des;
-extern const struct nettle_cipher nettle_openssl_cast128;
 
 extern const struct nettle_hash nettle_openssl_md5;
 extern const struct nettle_hash nettle_openssl_sha1;
